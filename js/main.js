7932716abe9b28cce3e307196b6754a7e7358f29
Lailakhatun <lailakhatun7699@gmail.com>
2021-09-16 01:32:21 +0600

    first

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//bx slider....
+$(document).ready(function(){
+      $('.slider-area').bxSlider();
+
+      $('#mean-menu').meanmenu({
+        meanMenuContainer: '#mobile-manu',
+        meanScreenWidth: 991,
+
+      });
+
+    });
